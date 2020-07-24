d75a3c9f000849e0a3758369c9ef3172c1028bbf
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-24 02:13:43 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,15 +1,8 @@
 class Cat
   
-  attr_accessor=:name
+  attr_accessor : name
   
-  def name=(name)
-    @name= name
+  def meow 
+    puts "meow!"
   end
-  
-  def name
-    @name
-  end
-  
-  
-
 end
